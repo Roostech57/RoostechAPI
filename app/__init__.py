5222f916bf4b8983d96956c7254bfe802375951a
Wilson Gallo <wilsonhenryg@gmail.com>
2025-05-25 20:34:54 -0500

    Fix import _init_.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -7,13 +8,21 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    # ✅ Cargar desde variable de entorno
+    # ✅ Configuración desde .env para base de datos
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    # ✅ Opciones de motor para mantener la conexión viva
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+        'pool_size': 10,
+        'max_overflow': 20,
+        'pool_timeout': 30,
+        'pool_recycle': 1800
+    }
+
     db.init_app(app)
 
-    # Registro de rutas
+    # Registro de blueprints de rutas
     from app.routes.factura_publica_routes import factura_publica_bp
     app.register_blueprint(factura_publica_bp)
 
@@ -21,5 +30,3 @@ def create_app():
     app.register_blueprint(producto_bp)
 
     return app
-
-
