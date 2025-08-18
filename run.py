a659c7a7c243ee74393b82ed9f0ae07e52a6f2f3
Wilson Gallo <wilsonhenryg@gmail.com>
2025-08-18 16:37:22 -0500

    fix: compatible run.py for Render deployment

--- run.py
+++ run.py
@@ -8,6 +8,3 @@ app = create_app()
 with app.app_context():
     db.create_all()
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host="0.0.0.0", port=port)
